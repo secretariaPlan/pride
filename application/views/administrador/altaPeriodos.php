bb6cfad84685a9db0845c43c11d74607256a17e0
secretariaPlan <secretaria.plan.informatica@gmail.com>
2015-02-18 22:52:32 -0600

    Las vistas alta periodo y asignacion se incluyeron en la carpeta administrador.

---
+++ application/views/administrador/altaPeriodos.php
@@ -0,0 +1,38 @@
+<body>
+    <div class="grid">
+        <div class="col_4"></div>
+        <div class="col_4">
+        	<br>
+        	<br>
+        	<div class="left">
+        		  <button class="blue">Nuevo Periodo</button>
+        	</div>
+            <div class="center">
+                    <fieldset>
+                    <br>
+					<label for="ano">A&ntilde;o</label>
+					<select id="ano">
+					<option value="0">-- Seleccione el A&ntilde;o --</option>
+					<option value="1">2015</option>
+					<option value="2">2016</option>
+					<option value="3">2017</option>
+					</select>
+					<br>
+					<br>
+					<label for="numero">N&uacute;mero</label>
+					<select id="numero">
+					<option value="0">-- Seleccione el N&uacute;mero --</option>
+					<option value="1">01</option>
+					<option value="2">02</option>
+					<option value="3">03</option>
+					</select>
+					<br>
+					<br>
+                    <button class="blue">Guardar Periodo</button>
+                    <br>
+                </fieldset>
+            </div>
+        </div>
+        <div class="col_4"></div>
+    </div>
+</body>
---
+++ application/views/administrador/asignacion.php
@@ -0,0 +1,67 @@
+<body>
+    <div class="grid">
+        <div class="col_4"></div>
+        <div class="col_4">
+
+        <div class="center">
+                    <fieldset>
+                    <br>
+					<label for="evaluador">Seleccionar Evaluador</label>
+					<br>
+					<select id="evaluador">
+					<option value="1">Profesor 1</option>
+					<option value="2">Profesor 2</option>
+					<option value="3">Profesor 3</option>
+					</select>
+					<br>
+					<br>
+					<label for="evaluado">Seleccionar Evaluado</label>
+					<br>
+					<select id="evaluado">
+					<option value="1">Profesor 4</option>
+					<option value="2">Profesor 5</option>
+					<option value="3">Profesor 6</option>
+					</select>
+					<br>
+					<br>
+                    <button class="blue">Agregar</button>
+                    <br>
+                </fieldset>
+            </div>
+        
+        
+        
+            <div class="center">
+                    <fieldset>
+                  	 	<table class="sortable" cellspacing="0" cellpadding="0">
+							<thead><tr class="alt first last">
+								<th rel="0" value="Profesor">Profesor</th>
+								<th rel="1" value="Opciones">Opcciones</th>
+							</tr></thead>
+							<tbody>
+								<tr class="">
+									<td value="Profesor 2">Profesor 2</td>
+									<td value=" 
+									"><a href=""><i class="fa fa-minus-square"></i></a></td>
+								</tr>
+								<tr class="">
+									<td value="Profesor 3">Profesor 3</td>
+									<td value=" 
+									"><a href=""><i class="fa fa-minus-square"></i></a></td>
+								</tr>
+								<tr class="">
+									<td value="Profesor 4">Profesor 4</td>
+									<td value=" 
+									"><a href=""><i class="fa fa-minus-square"></i></a></td>
+								</tr>
+							</tbody>
+						</table>
+						<br>
+	                    <button class="blue">Guardar Cambios</button>
+	                    <br>
+                	</fieldset>
+            </div>
+        </div>
+        <div class="col_4"></div>
+    </div>
+</body>
