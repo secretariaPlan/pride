a3b0dec27733f07232fed45d235c6991a7c0c826
secretariaPlan <secretaria.plan.informatica@gmail.com>
2015-02-21 17:25:07 -0600

    Agregados las funciones para nuevo evaluado y evaluador

--- application/models/pride/evaluado.php
+++ application/models/pride/evaluado.php
@@ -4,6 +4,17 @@ class Evaluado extends ActiveRecord\Model{
 	
 	static $table_name = "evaluado";
 	
+	function nuevoEvaluado($idUsuario,$idPeriodo,$idComision) {
+		
+		$evaluado = new Evaluado();
+		
+		$evaluado->id_usuario = $idUsuario;
+		$evaluado->id_periodo = $idPeriodo;
+		$evaluado->id_comision = $idComision;
+		
+		$evaluado->save();
+	}
+	
 }
 
 ?>
--- application/models/pride/evaluador.php
+++ application/models/pride/evaluador.php
@@ -4,6 +4,16 @@ class Evaluador extends ActiveRecord\Model{
 	
 	static $table_name = "evaluador";
 	
+	function nuevoEvaluador($idUsuario,$idPeriodo,$idComision) {
+	
+		$evaluador = new Evaluador();
+	
+		$evaluador->id_usuario = $idUsuario;
+		$evaluador->id_periodo = $idPeriodo;
+		$evaluador->id_comision = $idComision;
+	
+		$evaluador->save();
+	}
 }
 
 ?>
