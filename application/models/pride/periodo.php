4808327e76b25375475c196be0667ab24d567f8a
gabrrieldelabarrera <gabrieldelabarrera@gmail.com>
2015-02-23 13:41:17 -0600

    Modelo Periodo
    Agregé Modelo periodo como me lo pidio angel

--- application/models/pride/periodo.php
+++ application/models/pride/periodo.php
@@ -6,4 +6,20 @@ class Periodo extends ActiveRecord\Model{
 	
 }
 
+
+	function nuevoPeriodo($cPeriodo,$cNumSem) {
+		
+	
+				
+		$periodo= new Periodo();
+		
+		$periodo->periodo = $cPeriodo;
+		$periodo->num_sem = $cNumSem = array(
+		    'uno' => '1',
+		    'dos' => '2',
+		);
+		
+		$periodo->save();
+	}
+
 ?>
