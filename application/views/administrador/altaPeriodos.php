511121e8e4e60bb80fffb8eb692635755249f5b6
gabrrieldelabarrera <gabrieldelabarrera@gmail.com>
2015-03-05 13:41:42 -0600

    Quitar Botón Nuevo Periodo en un rango de fechas con Jquery
    A periodo.js se le agrego unas variables donde use dos div.
    El  primer  div tiene una fecha de inicio y otra fecha de fin el cual
    muestra el  botón y el contenido de lo que son las altas, este div se
    mostrara en el tiempo que se pueda dar de alta
    Agregué otro div tiene una fecha de inicio y fecha de fin este div
    mostrara un mensaje que en ese lapso no se puede dar de alta
    Para que aparecieran estos divs y se ocultaran en periodo.js use los
    eventos start y end  para fechas indicando en que tiempo se habilita y
    se oculta un div con el contenido que se quiere mostrar
    Cuando se  pulsa el boton nuevo periodo se muestra el cuadro y el botón
    desaparece, esto lo realice
    Nota: en el header quite el código del script de que manda a llamar a
    periodo.js y lo pase al final de la vista de altaperiodo, esto lo tuve
    que hacer porque si el jquery no estaba al final de la vista no
    realizaba las funciones necesarias el jquery.

--- application/views/administrador/altaPeriodos.php
+++ application/views/administrador/altaPeriodos.php
@@ -2,17 +2,70 @@
     <div class="grid">
         <div class="col_4"></div>
         <div class="col_4">
-        
-        
-            <div id="contenidoPeriodo"> 
-	            <br>
+
+				<br>
         		<br>
-	        	<div class="center">
-	        		  <button class="blue" id="NuevoPeriodo"" >Nuevo Periodo</button>
+        
+				        <!-- Periodo de Alta -->
+        <div id = "fechaAlta" style="display:none">
+        	
+        	<!-- boton -->
+        	   	<div class="center">
+	        		  <button class="blue" id="boton1">Nuevo Periodo</button>
 	        	</div>
-            </div>
- 
+        	<!-- Fin del Boton -->
+        	
+        	<!-- contenido -->
+        	 <div id="contenidoPeriodo" style="display: none;"> 
+	         	<div class="center">
+	                    <fieldset>
+	                    <br>
+						<label for="ano">A&ntilde;o</label>
+						<input id="ano" type="text" placeholder="Ingrese el A&ntilde;o">
+						<br>
+						<br>
+						<label for="numero">N&uacute;mero</label>
+						<select id="numero">
+						<option value="0">-- Seleccione el N&uacute;mero --</option>
+						<option value="1">1</option>
+						<option value="2">2</option>
+						</select>
+						<br>
+						<br>
+	                    <button class="blue">Guardar Periodo</button>
+	                    <br>
+	                </fieldset>
+	            </div>
+	         </div>
+        <!-- fin del contenido -->
+        
         </div>
-        <div class="col_4"></div>
+		<!-- Fin del periodo de alta -->
+		
+		
+		<!-- Fecha donde no se puede dar de alta -->
+		<div id = "fechaFin" style="display:none">
+			En este momento no puede dar de alta un nuevo periodo
+		</div>
+		<!-- Fin de la fecha donde no puede dar de alta -->
+        		
+        		
+       	
+       
+       
+       
+       
+       
+       
+       
+       </div> 		
+    
+    
     </div>
+
+
+
+        
+
 </body>
+<script type="text/javascript" src="<?= base_url() ?>js/periodo.js"></script>
--- js/periodo.js
+++ js/periodo.js
@@ -1,7 +1,24 @@
 
-$(function (){
-	$("#NuevoPeriodo").click(function(){
-		$("#contenidoPeriodo").load("../../administrador/contenidoPeriodo");
-	});
-	
-});
+		
+$(document).ready(function(){
+			$("#boton1").click(function(){
+				$("div[id=contenidoPeriodo]").css("display", "block");
+				$("#boton1").css("display", "none");
+			});
+
+		
+		});
+
+
+var today = new Date();
+
+//var endofseason = new Date("March 4, 2013"); //Fin de temporada del Div
+//if (today >= endofseason) {document.getElementById("div").style.display = "block"}
+
+var startEventDate = new Date("March 6, 2015 00:00"); // Fecha de inicio para que se muestre el div
+var endEventDate = new Date ("March 06, 2015 23:59"); // Fecha de Fin para que se muestre el div
+if ((today >= startEventDate)  && (today <=endEventDate)) {document.getElementById("fechaAlta").style.display = "block"}
+
+var startEventDate = new Date("March 5, 2015 00:00"); //Fecha de inicio para que se muestre el div
+var endEventDate = new Date ("March 05, 2015 23:59"); // Fecha de Fin para que se muestre el div
+if ((today >= startEventDate)  && (today <=endEventDate)) {document.getElementById("fechaFin").style.display = "block"}
