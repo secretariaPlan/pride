38c8eca143693860250600810129441ec6491ee7
gabrrieldelabarrera <gabrieldelabarrera@gmail.com>
2015-03-06 14:27:29 -0600

    Se creó la vista para recuperar contraseña
    Cree la vista para recuperar contraseña, para esto, cree un nuevo
    controller llamado recovery_pass y su vista llamada recovery_pass

--- application/controllers/recovery_pass.php
+++ application/controllers/recovery_pass.php
@@ -1,10 +1,10 @@
 <?php
 
-class Login extends CI_Controller {
+class Recovery_pass extends CI_Controller {
 	function index() {
 		$this->load->helper('url');
 		$this->load->view("header");
-		$this->load->view("login");
+		$this->load->view("recovery_pass");
 		$this->load->view("footer");
 	}
 
---
+++ application/views/recovery_pass.php
@@ -0,0 +1,36 @@
+<body>
+    <div class="grid">
+        <div class="col_4"></div>
+        <div class="col_4">
+        	
+        
+            <div class="center">
+            	
+            	<div class="notice warning">
+            		<i class="fa fa-warning fa-large"></i> 
+            			Ingresa tu correo electr&oacute;nico.
+				</div>
+            	
+            	
+                <fieldset>
+                    <label>Correo Electr&oacute;nico</label>
+                    <br>
+                    <input id="rfc" type="text" placeholder="Correo Electr&oacute;nico"/>
+                    <br>
+                    <br>
+                    <label>Repita su Correo Electr&oacute;nico</label>
+                    <br>
+                    <input id="pass" type="password" placeholder="Repita su Correo Electr&oacute;nico"/>
+                    <br>
+                    <br>
+                    <button class="blue">Enviar Contrase&ntilde;a</button>
+                    <br>
+                    <div class="col_12 right">
+                        <a href="<?php echo base_url()?>">Regresar</a>
+                    </div>
+                </fieldset>
+            </div>
+        </div>
+        <div class="col_4"></div>
+    </div>
+</body>
