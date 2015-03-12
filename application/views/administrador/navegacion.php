fa6be853bb91f21431a99312ad7ab07b325496ae
angelmoralesc <angel.morales.colin@gmail.com>
2015-03-12 00:06:01 -0600

    Se corrigieron los enlaces a los distintos apartados. La barra se centro en la pantalla.

--- application/views/administrador/navegacion.php
+++ application/views/administrador/navegacion.php
@@ -1,7 +1,8 @@
 <!-- Menu Horizontal -->
-
-<ul class="menu">
-<li><a href="<?php echo base_url().'administrador'?>">Asignaci&oacute;n de Profesores</a></li>
-<li><a href="<?php echo base_url().'administrador/altaProfesores'?>">Alta Profesores</a></li>
-<li><a href="<?php echo base_url().'administrador/altaPeriodos'?>">Alta Periodos</a></li>
-</ul>
+<div class="center">	
+	<ul class="menu">
+		<li><a href="<?php echo site_url('administrador')?>">Asignaci&oacute;n de Profesores</a></li>
+		<li><a href="<?php echo site_url('administrador/altaProfesores')?>">Alta Profesores</a></li>
+		<li><a href="<?php echo site_url('administrador/altaPeriodos')?>">Alta Periodos</a></li>
+	</ul>
+</div>	
