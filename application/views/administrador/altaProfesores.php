cddf52512886245e70e8aa4aaa01c44fe56458a4
angelmoralesc <angel.morales.colin@gmail.com>
2015-03-12 23:34:39 -0600

    Se agregan elementos a la  vista altaProfesores

--- application/views/administrador/altaProfesores.php
+++ application/views/administrador/altaProfesores.php
@@ -1,6 +1,34 @@
 <body>
     <div class="grid flex">
+        
+        <div class="col_4"></div>
+        <div class="col_4 center">
+            
+            <button class="blue" id="cargar" name="cargar">Cargar archivo</button>
+        </div>
         <div class="col_4"></div>
        
+        <div class="clear fix"></div>
+        
+        <div class="col_2"></div>
+        <div class="col_8">
+            <table>
+                <thead>
+                    <th>
+                        <td>RFC</td>
+                        <td>Nombre</td>
+                        <td>Apellido paterno</td>
+                        <td>Apellido materno</td>
+                        <td>Correo electronico</td>
+                        <td>Opciones</td>
+                    </th>
+                </thead>
+            
+                <tbody id="datos">
+            
+                </tbody>
+            </table>
+        </div>
+        <div class="col_2"></div>
     </div>
 </body>
