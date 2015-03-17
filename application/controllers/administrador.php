8e59c39d31a43d8209972f09b0cb0e6e871416f4
angelmoralesc <angel.morales.colin@gmail.com>
2015-03-16 22:42:32 -0600

    Se agrega la carga de archivos csv

--- application/controllers/administrador.php
+++ application/controllers/administrador.php
@@ -24,6 +24,7 @@ class Administrador extends CI_Controller {
 	}
 	
 	function altaProfesores() {
+		$this->load->helper('form');
 		$this->load->helper('url');
 		$this->load->view("header");
 		$this->load->view("administrador/navegacion");
@@ -31,13 +32,34 @@ class Administrador extends CI_Controller {
 		$this->load->view("footer");
 	}
 	
-	function nuevoProfesor(){
-		$this->load->helper('url');
-		$this->load->view("header");
-		$this->load->view("administrador/navegacion");
-		$this->load->view("administrador/nuevoProfesor");
-		$this->load->view("footer");
+	public function cargarCsv() {
+		$this->load->helper('form');
+		$config['upload_path'] = './subidas/';
+		$config['allowed_types'] = 'csv';
+		$this->load->library('upload', $config);
 		
+		if(!$this->upload->do_upload()){
+			$error=array('error' => $this->upload->display_errors());
+			$this->load->helper('url');
+			$this->load->view("header");
+			$this->load->view("administrador/navegacion");
+			$this->load->view('administrador/altaProfesores', $error);
+			$this->load->view("footer");
+		}else {
+			
+			$this->load->library('csvreader');
+			$datos=$this->upload->data();
+			//print_r($datos);
+			$ruta = $datos["full_path"];
+			$datosCsv["datos"] = $this->csvreader->parse_file($ruta);
+			//print_r($datosCsv);			
+			
+			$this->load->helper('url');
+			$this->load->view("header");
+			$this->load->view("administrador/navegacion");
+			$this->load->view("administrador/altaProfesores",$datosCsv);
+			$this->load->view("footer");
+		}		
 	}
 
 }
