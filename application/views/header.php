3f261667143abbf0dea93fdfb717a968827b1aae
secretariaPlan <secretaria.plan.informatica@gmail.com>
2015-04-05 23:58:12 -0500

    Se actualizan las llamadas a jquery

--- application/views/header.php
+++ application/views/header.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+        <script src="<?php echo base_url();?>jquery/jquery-ui-1.9.2.custom.js"/></script>
         <script src="<?php echo base_url();?>js/kickstart.js" ></script>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>css/kickstart.css" media="all">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>css/pride.css" media="all">
