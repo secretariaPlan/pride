75fec3f43ec9d3f99f0af15f294fcda94fea2b3a
angelmoralesc <angel.morales.colin@gmail.com>
2015-04-11 18:21:08 -0500

    Correccion al modelo EvaluadorEvaluado

--- application/models/pride/evaluadorEvaluado.php
+++ application/models/pride/evaluadorEvaluado.php
@@ -14,17 +14,19 @@ class EvaluadorEvaluado extends ActiveRecord\Model{
 		$evaluador_evaluado->save();
 	}
 	
+	function desasignar($idEvaluador,$idEvaluado) {
+	
+	
+		$evaluador_evaluado = evaluador_evaluado::first(array("conditions" => array("id_evaluador = ? AND id_evaluado = ?",$idEvaluador,$idEvaluado) ));
+		$evaluador_evaluado->delete();
+	
+	
+	}
+	
 }
 
 
-function desasignar($idEvaluador,$idEvaluado) {
 
-
-	$evaluador_evaluado = evaluador_evaluado::first(array("conditions" => array("id_evaluador = ? AND id_evaluado = ?",$idEvaluador,$idEvaluado) ));
-	$evaluador_evaluado->delete();
-
-
-}
 
 
 
