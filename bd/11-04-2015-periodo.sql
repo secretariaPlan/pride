a105d3f58e727d974d9e159f0e12be8f71cdcd22
angelmoralesc <angel.morales.colin@gmail.com>
2015-04-12 21:26:27 -0500

    Modificacion a la tabla periodo

--- bd/11-04-2015-periodo.sql
+++ bd/11-04-2015-periodo.sql
@@ -4,13 +4,11 @@ CREATE TABLE IF NOT EXISTS `periodo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `year` varchar(4) NOT NULL,
   `numero` enum('1','2') NOT NULL,
-  `inicioPer` date NOT NULL,
-  `finPer` date NOT NULL,
-  `inicioEval` date NOT NULL,
-  `finEval` date NOT NULL,
-  `inicioEntrega` date NOT NULL,
-  `finEntrega` date NOT NULL,
+  `inicioper` date NOT NULL,
+  `finper` date NOT NULL,
+  `inicioeval` date NOT NULL,
+  `fineval` date NOT NULL,
+  `inicioentrega` date NOT NULL,
+  `finentrega` date NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-
