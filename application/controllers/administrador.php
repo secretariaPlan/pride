1647c3ea915f138c5f240d783d7a1a67bf7593a2
gabrrieldelabarrera <gabrieldelabarrera@gmail.com>
2015-05-06 10:37:54 -0500

    Pantalla Bienvenido
    Se creo la pantalla bienvenido para que se muestre enseguida de que el
    usuario ingrese

--- application/controllers/administrador.php
+++ application/controllers/administrador.php
@@ -12,9 +12,18 @@ class Administrador extends CI_Controller {
 	function index() {
 		$this->load->view("header");
 		$this->load->view("administrador/navegacion");
+		$this->load->view("administrador/bienvenido");
+		$this->load->view("footer");
+	}
+	
+	
+	function asignacion() {
+		$this->load->view("header");
+		$this->load->view("administrador/navegacion");
 		$this->load->view("administrador/asignacion");
 		$this->load->view("footer");
 	}
+	
 	
 	function altaPeriodos() {
 		$this->load->view("header");
