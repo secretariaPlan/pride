25e26f85a964d09d9730095fcd0cc65fa3d3c021
angelmoralesbosc <angel.morales@bosc.com.mx>
2015-06-10 17:03:37 -0500

    Correcciones en la estructura.

--- js/periodo.js
+++ js/periodo.js
@@ -66,19 +66,8 @@ function desasignar(id){
     });
 }
 
-$(document).ready(function () {
-    
-    $("#periodo").on("change keyup copy paste cut", function(){
-        
-        if($("#periodo").val().length < 5 && $('input[name="id"]').val() ){
-            $("#periodo").val("");
-            $('input[name="id"]').val("");
-            $('input[name="nombrePeriodo"]').val("");
-            $("#periodos").html("");     
-        }
-        
-    });
-    
+$(document).ready(function(){
+    $(".fecha").datepicker({dateFormat: "dd-mm-yy"});
     
     listaPeriodoUltimo();
 
@@ -100,30 +89,6 @@ $(document).ready(function () {
                 },500);
         
     });
-    
-});
-$(document).ready(function(){
-    $(".fecha").datepicker({dateFormat: "dd-mm-yy"});
-/*    
-			$("#boton1").click(function(){
-				$("div[id=contenidoPeriodo]").css("display", "block");
-				$("#boton1").css("display", "none");
-			});
-*/
+
 		
 });
-
-/*
-var today = new Date();
-
-//var endofseason = new Date("March 4, 2013"); //Fin de temporada del Div
-//if (today >= endofseason) {document.getElementById("div").style.display = "block"}
-
-var startEventDate = new Date("March 7, 2015 00:00"); // Fecha de inicio para que se muestre el div
-var endEventDate = new Date ("May 07, 2015 23:59"); // Fecha de Fin para que se muestre el div
-if ((today >= startEventDate)  && (today <=endEventDate)) {document.getElementById("fechaAlta").style.display = "block"}
-
-var startEventDate = new Date("May 8, 2015 00:00"); //Fecha de inicio para que se muestre el div
-var endEventDate = new Date ("May 10, 2015 23:59"); // Fecha de Fin para que se muestre el div
-if ((today >= startEventDate)  && (today <=endEventDate)) {document.getElementById("fechaFin").style.display = "block"}
-*/
