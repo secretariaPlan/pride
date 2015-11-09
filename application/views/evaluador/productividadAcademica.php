9b87f667bce680ea932e9aba762b13316ab781aa
Angel Morales <angel.morales.colin@gmail.com>
2015-11-08 22:36:50 -0600

    Se agregan los dialogos emergentes para los comentarios en cada una de las secciones del evaluador

--- application/views/evaluador/productividadAcademica.php
+++ application/views/evaluador/productividadAcademica.php
@@ -1,3 +1,4 @@
+<script src="<?php echo base_url();?>js/evaluador/prodAca.js" ></script>
 <body>
     <div class="grid flex">
     	<div class="col_12">
@@ -68,6 +69,17 @@
                             <td width="20%">Valor</td>
                         </tr>
                     </table>
+                    <div class="col_12 center">
+                        <button id="botonComentario" class="medium blue">Agregar comentario</button>
+                    </div>
+                    <div id="comentario" title="Agregar comentario">
+                        <div class="col_12">
+                            <textarea id="texto" class="col_12" placeholder="Agregue un comentario"></textarea>
+                        </div>
+                        <div class="12 center">
+                            <button class="small green">Guardar</button>
+                        </div>
+                    </div>
                 </fieldset>
             </div>
             <div class="col_2"></div>
---
+++ js/evaluador/docencia.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#comentario").hide();
+    $("#botonComentario").click(function(){
+        $("#comentario").dialog({
+            height:350,
+            width:600,
+            show: { effect: 'drop', direction: "up" },
+            modal:true
+        });
+    });
+});
