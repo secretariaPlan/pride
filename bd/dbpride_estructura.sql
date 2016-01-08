fea28b3cceed9131fae9ddeacb02dd4ad19b99a8
Angel Morales <angel.morales.colin@gmail.com>
2016-01-08 01:19:12 -0600

    respaldo de la estructura de dbpride

---
+++ bd/dbpride_estructura.sql
@@ -0,0 +1,245 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.13.1deb1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 07-01-2016 a las 14:55:32
+-- Versión del servidor: 5.6.27-0ubuntu1
+-- Versión de PHP: 5.6.11-1ubuntu3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `dbpride`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `administrador`
+--
+
+CREATE TABLE IF NOT EXISTS `administrador` (
+  `id` int(11) NOT NULL,
+  `rfc` varchar(14) NOT NULL,
+  `password` varchar(400) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `apaterno` varchar(45) NOT NULL,
+  `amaterno` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comision`
+--
+
+CREATE TABLE IF NOT EXISTS `comision` (
+  `id` int(11) NOT NULL,
+  `comision` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `evaluado`
+--
+
+CREATE TABLE IF NOT EXISTS `evaluado` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_periodo` int(11) NOT NULL,
+  `id_comision` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `evaluador`
+--
+
+CREATE TABLE IF NOT EXISTS `evaluador` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_periodo` int(11) NOT NULL,
+  `id_comision` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `evaluador_evaluado`
+--
+
+CREATE TABLE IF NOT EXISTS `evaluador_evaluado` (
+  `id` int(11) NOT NULL,
+  `id_evaluador` int(11) NOT NULL,
+  `id_evaluado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `periodo`
+--
+
+CREATE TABLE IF NOT EXISTS `periodo` (
+  `id` int(11) NOT NULL,
+  `year` varchar(4) NOT NULL,
+  `numero` enum('1','2') NOT NULL,
+  `inicioper` date NOT NULL,
+  `finper` date NOT NULL,
+  `inicioeval` date NOT NULL,
+  `fineval` date NOT NULL,
+  `inicioentrega` date NOT NULL,
+  `finentrega` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(11) NOT NULL,
+  `rfc` varchar(45) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `apaterno` varchar(45) NOT NULL,
+  `amaterno` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `correo` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  ADD PRIMARY KEY (`id`,`rfc`);
+
+--
+-- Indices de la tabla `comision`
+--
+ALTER TABLE `comision`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `evaluado`
+--
+ALTER TABLE `evaluado`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_periodo` (`id_periodo`),
+  ADD KEY `id_comision` (`id_comision`);
+
+--
+-- Indices de la tabla `evaluador`
+--
+ALTER TABLE `evaluador`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_periodo` (`id_periodo`),
+  ADD KEY `id_comision` (`id_comision`);
+
+--
+-- Indices de la tabla `evaluador_evaluado`
+--
+ALTER TABLE `evaluador_evaluado`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_evaluador` (`id_evaluador`),
+  ADD KEY `id_evaluado` (`id_evaluado`),
+  ADD KEY `id_evaluador_2` (`id_evaluador`),
+  ADD KEY `id_evaluado_2` (`id_evaluado`);
+
+--
+-- Indices de la tabla `periodo`
+--
+ALTER TABLE `periodo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`,`rfc`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `comision`
+--
+ALTER TABLE `comision`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `evaluado`
+--
+ALTER TABLE `evaluado`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `evaluador`
+--
+ALTER TABLE `evaluador`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `evaluador_evaluado`
+--
+ALTER TABLE `evaluador_evaluado`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `periodo`
+--
+ALTER TABLE `periodo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `evaluado`
+--
+ALTER TABLE `evaluado`
+  ADD CONSTRAINT `comision_evaluado_fk` FOREIGN KEY (`id_comision`) REFERENCES `comision` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `periodo_evaluado_fk` FOREIGN KEY (`id_periodo`) REFERENCES `periodo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `usuario_evaluado_fk` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `evaluador`
+--
+ALTER TABLE `evaluador`
+  ADD CONSTRAINT `comision_evaluador_fk` FOREIGN KEY (`id_comision`) REFERENCES `comision` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `periodo_evaluador_fk` FOREIGN KEY (`id_periodo`) REFERENCES `periodo` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `usuario_evaluador_fk` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `evaluador_evaluado`
+--
+ALTER TABLE `evaluador_evaluado`
+  ADD CONSTRAINT `evaluado_eval_fk` FOREIGN KEY (`id_evaluado`) REFERENCES `evaluado` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `evaluador_eval_fk` FOREIGN KEY (`id_evaluador`) REFERENCES `evaluador` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
